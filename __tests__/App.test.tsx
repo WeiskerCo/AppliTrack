24a1754efcb9eaa3051b4bd2896c2dae9541e1ea
Matthew Weisker <mweisker@live.com>
2023-02-28 12:32:33 -0800

    Set up testing boilerplate

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import '@testing-library/jest-dom';
+// import Prism from 'prismjs';
+// import { describe, expect, test } from '@jest/globals'
+
+
+import App from '../client/App';
+
+
+describe('quick maffs test suite', () => {
+  test('2 + 2 is 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
