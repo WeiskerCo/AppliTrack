55c907b4490dea15bf01def29192fe75405dccd4
Justin Kim <justin27kim@gmail.com>
2023-02-27 17:13:56 -0800

    set up navbar with react router
    Co-authored-by: TrevorJFerguson <TrevorJFerguson@users.noreply.github.com>

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const container = document.getElementById('root');
@@ -8,7 +8,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 )
