a43bbd746dc2f6b45c7fc22f1a9b5ef390210aeb
Justin Kim <justin27kim@gmail.com>
2023-03-01 10:48:34 -0800

    pushing to dev

--- client/index.tsx
+++ client/index.tsx
@@ -15,7 +15,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <GoogleOAuthProvider clientId='29448142204-hghum62l1h5mi4b0g3rouepdlr4por8q.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId='Google Oauth'>
     <BrowserRouter>
       <App />
     </BrowserRouter>
